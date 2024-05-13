8928cd264b3e760df799d13617ed75be96179427
JustinJira <jus.jira@gmail.com>
2024-05-13 13:53:14 +0700

    feat: read sensor number from topic

--- ESP32/queue_hts221.ino
+++ ESP32/queue_hts221.ino
@@ -13,7 +13,7 @@ const char* WIFI_PASSWORD = "iotgroup5";
 const char* ntpServer = "pool.ntp.org";
 
 // MQTT topics
-#define topicToPublish   "@msg/data/3"
+#define topicToPublish "@msg/data/1"
 #define topicToSubscribe "@msg/cc"
 
 // MQTT-Connection
@@ -32,8 +32,8 @@ const char* mqtt_server = "raspi.lan";
 const int mqtt_port = 1883;
 
 // Built-in RGB LED
-#define PIN 18 // not sure
-#define NUMPIXELS 1 // only 1 RGB LED is on Cucumber board 
+#define PIN 18       // not sure
+#define NUMPIXELS 1  // only 1 RGB LED is on Cucumber board
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void reconnect() {
@@ -42,7 +42,9 @@ void reconnect() {
   while (!mqtt.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (mqtt.connect(mqtt_server)) {
+    String clientId = "ESP32-";
+    clientId += String(random(0xffff), HEX);
+    if (mqtt.connect(clientId.c_str())) {
       Serial.println("connected");
       mqtt.subscribe(topicToSubscribe);
       Serial.print("Subscribed to ");
@@ -89,7 +91,7 @@ void setupHardware() {
 
 void printLocalTime() {
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
+  if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
     return;
   }
@@ -97,25 +99,25 @@ void printLocalTime() {
 }
 
 void taskDisplayTime(void* parameter) {
-  for(;;) {
+  for (;;) {
     printLocalTime();
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
 void taskUpdateTime(void* parameter) {
-  for(;;) {
-    configTime(7*3600, 0, ntpServer); // +7 UTC Thailand Time
+  for (;;) {
+    configTime(7 * 3600, 0, ntpServer);  // +7 UTC Thailand Time
     Serial.println("NTP updated");
-    vTaskDelay(60000/portTICK_PERIOD_MS);
+    vTaskDelay(60000 / portTICK_PERIOD_MS);
   }
 }
 
-void sendSensorData(void *parameter) {
+void sendSensorData(void* parameter) {
   for (;;) {
     sensors_event_t temp, humidity;
     hts.getEvent(&humidity, &temp);
-    
+
     float temp_hts = temp.temperature;
     float humid_hts = humidity.relative_humidity;
     float pressure_bmp = bmp.readPressure();
@@ -123,21 +125,21 @@ void sendSensorData(void *parameter) {
     char json_body[200];
 
     const char json_tmpl[] = "{\"temperature\": %.2f,"
-                             "\"humidity\": %.2f," 
+                             "\"humidity\": %.2f,"
                              "\"pressure\": %.2f}";
-    // sprintf(var, format, arg);  %.2f = round the float to two decimal places                   
+    // sprintf(var, format, arg);  %.2f = round the float to two decimal places
     sprintf(json_body, json_tmpl, temp_hts, humid_hts, pressure_bmp);
 
-    Serial.println(json_body); //json message is a string
-        
+    Serial.println(json_body);  //json message is a string
+
     // Queue JSON message to the back of the FIFO queue
     xQueueSendToBack(sensorDataQueue, &json_body, portMAX_DELAY);
 
-    vTaskDelay(pdMS_TO_TICKS(60000)); // Read sensor data every minute
+    vTaskDelay(pdMS_TO_TICKS(60000));  // Read sensor data every minute
   }
 }
 
-void publishDataFromQueue(void *parameter) {
+void publishDataFromQueue(void* parameter) {
   char json_body[200];
 
   for (;;) {
@@ -156,7 +158,7 @@ void publishDataFromQueue(void *parameter) {
     }
 
     // Delay for a short time before checking the queue again
-    vTaskDelay(pdMS_TO_TICKS(60000)); // Publish every minute
+    vTaskDelay(pdMS_TO_TICKS(60000));  // Publish every minute
   }
 }
 
@@ -177,7 +179,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     jsonMessage[i] = (char)payload[i];
   }
 
-  Serial.println(jsonMessage); 
+  Serial.println(jsonMessage);
 
   //Extract the numbers from JSON message
   String str = String(jsonMessage);
@@ -199,12 +201,20 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // Get current temp & humidity value to check the model accuracy
   sensors_event_t temp, humidity;
   hts.getEvent(&humidity, &temp);
-  
+
   float temp_hts = temp.temperature;
   float humid_hts = humidity.relative_humidity;
-    
+
   // Check sensor No.
-  if(int(valToCompare[0]) == 3){
+  // Convert the C-style string to a String object
+  String messageString = String(topicToPublish);
+
+  // Find the position of the last '/'
+  int lastSlashIndex = messageString.lastIndexOf('/');
+
+  // Extract the substring containing the number
+  String numberString = messageString.substring(lastSlashIndex + 1);
+  if (int(valToCompare[0]) == numberString.toInt()) {
     Serial.println("Yep, it's mine");
     // Check predicted temperature & humidity accuracy
     // 00
@@ -215,40 +225,40 @@ void callback(char* topic, byte* payload, unsigned int length) {
     //^ if temp is inaccurate => yellow
     // 11
     //* if temp & humid are both accurate => green
-    
+
     float tempDiff = abs(valToCompare[1] - temp_hts);
     float humidDiff = abs(valToCompare[2] - humid_hts);
     // LED turns...
-    if((tempDiff > 10) && (humidDiff > 10)){
+    if ((tempDiff > 10) && (humidDiff > 10)) {
       // red
       pixels.setPixelColor(0, pixels.Color(150, 0, 0));
 
-    }else if ((tempDiff <= 10) && (humidDiff > 10)){
+    } else if ((tempDiff <= 10) && (humidDiff > 10)) {
       // blue
       pixels.setPixelColor(0, pixels.Color(0, 0, 150));
 
-    }else if ((tempDiff > 10) && (humidDiff <= 10)){
+    } else if ((tempDiff > 10) && (humidDiff <= 10)) {
       // yellow
       pixels.setPixelColor(0, pixels.Color(150, 150, 0));
 
-    }else{
+    } else {
       // green
       pixels.setPixelColor(0, pixels.Color(0, 150, 0));
     }
-    pixels.show();   // Send the updated pixel colors to the hardware.
-  }else{
+    pixels.show();  // Send the updated pixel colors to the hardware.
+  } else {
     Serial.println("Nope, discard the message pls");
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  pixels.begin(); // RGB LED
-  pixels.clear(); // Set all pixel colors to 'off'
+  pixels.begin();  // RGB LED
+  pixels.clear();  // Set all pixel colors to 'off'
   setupNetwork();
   setupHardware();
   mqtt.setServer(mqtt_server, mqtt_port);
-  mqtt.setCallback(callback); //set a callback function name "callback" to be called when a message arrives from a subscribed topic
+  mqtt.setCallback(callback);  //set a callback function name "callback" to be called when a message arrives from a subscribed topic
   sensorDataQueue = xQueueCreate(10, sizeof(char[200]));
   xTaskCreatePinnedToCore(
     taskDisplayTime,
@@ -263,24 +273,24 @@ void setup() {
     "Update Time",
     10000,
     NULL,
-    2, // Higher
+    2,  // Higher
     NULL,
     0);
   xTaskCreate(
-    sendSensorData,     // Task function
-    "sendSensorData",   // Task name
-    10000,              // Stack size (bytes)
-    NULL,               // Parameter to pass to the task
-    3,                  // Task priority
-    NULL                // Task handle
+    sendSensorData,    // Task function
+    "sendSensorData",  // Task name
+    10000,             // Stack size (bytes)
+    NULL,              // Parameter to pass to the task
+    3,                 // Task priority
+    NULL               // Task handle
   );
   xTaskCreate(
-    publishDataFromQueue,     // Task function
-    "publishDataFromQueue",   // Task name
-    10000,                    // Stack size (bytes)
-    NULL,                     // Parameter to pass to the task
-    4,                        // Task priority
-    NULL                      // Task handle
+    publishDataFromQueue,    // Task function
+    "publishDataFromQueue",  // Task name
+    10000,                   // Stack size (bytes)
+    NULL,                    // Parameter to pass to the task
+    4,                       // Task priority
+    NULL                     // Task handle
   );
 }
 
